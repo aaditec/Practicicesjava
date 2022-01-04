fd25c0a1ba3df6dfbaab0757fe9577d3abc79d80
aaditya <roshangautam213@gmail.com>
2022-01-04 15:58:50 +0545

    2nd

--- app/src/main/java/com/example/java_android/MainActivity.java
+++ app/src/main/java/com/example/java_android/MainActivity.java
@@ -14,16 +14,40 @@ TextView textView;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         textView = findViewById(R.id.textview);
 
-        textView.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                textView.setText("Hello Roshan");
+//        public void clickedOne(View view) {
+//            primaryTextView.setText(primaryTextView.getText() + "1");
+//        }
 
-                Toast.makeText(MainActivity.this,"welcome baby", Toast.LENGTH_SHORT).show();
-            }
-        });
 
+    }
+    public  void one(View view){
+        textView.setText(textView.getText()+"1");
+    }
+    public  void two(View view){
+        textView.setText(textView.getText()+"2");
+    }
+    public  void three(View view){
+        textView.setText(textView.getText()+"3");
+    }
+    public  void four(View view){
+        textView.setText(textView.getText()+"4");
+    }
+    public  void five(View view){
+        textView.setText(textView.getText()+"5");
+    }
+    public  void six(View view){
+        textView.setText(textView.getText()+"6");
+    }
+    public  void seven(View view){
+        textView.setText(textView.getText()+"7");
+    }
+    public  void eight(View view){
+        textView.setText(textView.getText()+"8");
+    }
+    public  void nine(View view){
+        textView.setText(textView.getText()+"9");
     }
 }
