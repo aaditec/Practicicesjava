8a77cf03015fb06ef596fb7219ec35f6bfdbd8c5
aaditya <roshangautam213@gmail.com>
2022-01-04 12:07:00 +0545

    1st

--- app/src/main/java/com/example/java_android/MainActivity.java
+++ app/src/main/java/com/example/java_android/MainActivity.java
@@ -3,12 +3,27 @@ package com.example.java_android;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textview);
+
+        textView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText("Hello Roshan");
+
+                Toast.makeText(MainActivity.this,"welcome baby", Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
